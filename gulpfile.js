1be78f11a978913395a4e6aa90a598ad3fd0bc6c
Maikel van Veen <maikeloo10@hotmail.com>
2018-11-10 18:58:37 +0100

    fix(gulp): remove semicolons

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,24 @@
-const gulp = require('gulp');
+const gulp = require('gulp')
 
 const browserSync = require('browser-sync').create(),
-    reloadBrowser = browserSync.reload;
+    reloadBrowser = browserSync.reload
 
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')
 
 const plumber = require('gulp-plumber'),
     clean = require('gulp-clean'),
     minifyImage = require('gulp-imagemin'),
     rename = require('gulp-rename'),
-    merge = require('merge-stream');
+    merge = require('merge-stream')
 
 const autoPrefixer = require('gulp-autoprefixer'),
-    minifyCSS = require('gulp-cssnano');
+    minifyCSS = require('gulp-cssnano')
 
 const babel = require('gulp-babel'),
-    minifyJS = require('gulp-uglify');
+    minifyJS = require('gulp-uglify')
 
-let assetsDir = './src';
-let distDir = './dist';
+let assetsDir = './src'
+let distDir = './dist'
 
 gulp.task('ppCSS', () => {
     return gulp.src(`${assetsDir}/css/style.scss`)
@@ -32,8 +32,8 @@ gulp.task('ppCSS', () => {
         }))
         .pipe(rename('style.min.css'))
         .pipe(gulp.dest(`${distDir}/css`))
-        .pipe(browserSync.stream());
-});
+        .pipe(browserSync.stream())
+})
 
 gulp.task('ppJS', () => {
     return gulp.src(`${assetsDir}/js/main.js`)
@@ -46,39 +46,39 @@ gulp.task('ppJS', () => {
         .pipe(minifyJS())
         .pipe(rename('main.min.js'))
         .pipe(gulp.dest(`${distDir}/js`))
-        .pipe(browserSync.stream());
-});
+        .pipe(browserSync.stream())
+})
 
 gulp.task('watch', ['ppCSS', 'ppJS'], () => {
     browserSync.init({
         server: "./",
         port: 4000,
-    });
+    })
 
 
-    gulp.watch(`./**/*.html`, reloadBrowser);
-    gulp.watch(`${assetsDir}/css/style.scss`, ['ppCSS']);
-    gulp.watch(`${assetsDir}/js/main.js`, ['ppJS']);
-});
+    gulp.watch(`./**/*.html`, reloadBrowser)
+    gulp.watch(`${assetsDir}/css/style.scss`, ['ppCSS'])
+    gulp.watch(`${assetsDir}/js/main.js`, ['ppJS'])
+})
 
 gulp.task('cleanImages', () => {
     gulp.src(`${distDir}/img`)
         .pipe(clean())
-});
+})
 
 gulp.task('ppImages', () => {
     gulp.src(`${assetsDir}/img/**/*`, {
             base: `${assetsDir}/img/`
         })
         .pipe(minifyImage())
-        .pipe(gulp.dest(`${distDir}/img`));
-});
+        .pipe(gulp.dest(`${distDir}/img`))
+})
 
 gulp.task('cleanDist', () => {
     gulp.src(`${distDir}`)
         .pipe(clean())
-});
+})
 
 gulp.task('buildDist', ['ppCSS', 'ppJS', 'ppImages'], () => {
-    console.log('Done');
-});
+    console.log('Done')
+})
